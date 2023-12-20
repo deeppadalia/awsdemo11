1740603f3d1d6c11528946fb10198a212c9f52e9
Deep Padalia <113300353+deeppadalia@users.noreply.github.com>
2023-12-20 13:24:02 +0530

    Update sum_of_digits.c

--- sum_of_digits.c
+++ sum_of_digits.c
@@ -1,14 +1,7 @@
 #include<stdio.h>
 void main()
 {
-	int n,sum=0,rem;
-	printf("enter value of n=");
+	printf("enter value");
 	scanf("%d",&n);
-	while(n>0)
-	{
-		rem=n%10;
-		sum=sum+rem;
-		n=n/10;
-	}
-	printf("%d",sum);
+	printf("%d",n);
 }
